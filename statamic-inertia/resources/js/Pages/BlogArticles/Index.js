b7af39666abfffef50a333f296a0c1d195ab3a04
Jonny Longfellow <jonny@parall.ax>
2023-03-01 13:10:04 +0000

    feat: adding blog articles

---
+++ statamic-inertia/resources/js/Pages/BlogArticles/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Blog({ title, ready, entry }) {
+    console.log(entry);
+    const { author } = entry;
+    console.log("author:%o", author);
+    return (
+        <div className="flex-1 flex justify-between flex-col h-full">
+            <p>{entry.title}</p>
+            <div>{entry.content}</div>
+            {author.name}
+            <img src={author.} alt="" />
+        </div>
+    );
+}
